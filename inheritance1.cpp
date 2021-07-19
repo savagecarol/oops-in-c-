e782acb232bc34828aa8769a5e29db34d7fe4fa0
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-07-19 09:24:10 +0530

    inheritance

---
+++ inheritance1.cpp
@@ -0,0 +1,59 @@
+// inheritance basic
+// method to inherit the feautures of one class to another class
+
+
+// accesss-----> technique 
+
+// rules for public 
+     //public    ------> public
+     //protected ------> protected
+
+// rules for private
+     //public    ------> private
+     //protected ------> private
+
+// rules for protected
+     //public    ------> protected
+     //protected ------> protected
+
+
+
+#include<bits/stdc++.h>
+using namespace std ;
+
+// class A is base class
+class A 
+{
+private:
+   static int p;  
+public: 
+    int age ;
+    string name ;
+    void setName(string iname){name = iname;}
+    void setAge(int iage){name = iage;}
+};
+
+
+// class B is sub class
+class B : public A  // accessing in public style
+{
+    public:
+        int id;
+        void setId(int iid){id = iid;}
+        void introduce()
+        {
+            cout  << name << " " << age << " " << id << endl;
+        }
+
+
+};
+
+int main()
+{
+    B savage;
+    savage.setName("savagecarol"); //  function of base class
+    savage.setAge(18); // function of base class
+    savage.setId(2); // function of same class
+    savage.introduce();    
+    return 0;
+}
